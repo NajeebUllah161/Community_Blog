85681c17129b8d0a967e4d27e15a4d72342cc68b
NAJEEB <najeebkhannk123@yahoo.com>
2022-02-02 18:04:58 +0500

    recyclewView stuck

--- app/src/main/java/com/example/communityfeedapp/MainActivity.java
+++ app/src/main/java/com/example/communityfeedapp/MainActivity.java
@@ -1,16 +1,77 @@
 package com.example.communityfeedapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.communityfeedapp.databinding.ActivityMainBinding;
+import com.example.communityfeedapp.fragments.AddPostFragment;
+import com.example.communityfeedapp.fragments.HomeFragment;
+import com.example.communityfeedapp.fragments.NotificationFragment;
+import com.example.communityfeedapp.fragments.ProfileFragment;
+import com.example.communityfeedapp.fragments.SearchFragment;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+    ActionBar actionBar;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
 
+        actionBar = getSupportActionBar();
+        Objects.requireNonNull(actionBar).setTitle("Home");
+
+        // When we open the application first
+        // time the fragment should be shown to the user
+        // in this case it is home fragment
+        FragmentTransaction defaultFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        defaultFragmentTransaction.replace(R.id.container_framelayout, new HomeFragment(), "");
+        defaultFragmentTransaction.commit();
+
+        binding.bottomNavigationBar.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+            switch (itemId) {
+                case R.id.item_home:
+                    actionBar.setTitle("Home");
+                    fragmentTransaction.replace(R.id.container_framelayout, new HomeFragment());
+                    fragmentTransaction.commit();
+                    return true;
+                case R.id.item_notification:
+                    actionBar.setTitle("Notification");
+                    fragmentTransaction.replace(R.id.container_framelayout, new NotificationFragment());
+                    fragmentTransaction.commit();
+                    return true;
+                case R.id.item_add_post:
+                    actionBar.setTitle("Add Post");
+                    fragmentTransaction.replace(R.id.container_framelayout, new AddPostFragment());
+                    fragmentTransaction.commit();
+                    return true;
+                case R.id.item_search:
+                    actionBar.setTitle("Search");
+                    fragmentTransaction.replace(R.id.container_framelayout, new SearchFragment());
+                    fragmentTransaction.commit();
+                    return true;
+                case R.id.item_userprofile:
+                    actionBar.setTitle("Profile");
+                    fragmentTransaction.replace(R.id.container_framelayout, new ProfileFragment());
+                    fragmentTransaction.commit();
+                    return true;
+            }
+            return false;
+        });
     }
 }
---
+++ app/src/main/java/com/example/communityfeedapp/fragments/HomeFragment.java
@@ -0,0 +1,37 @@
+package com.example.communityfeedapp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.communityfeedapp.R;
+import com.example.communityfeedapp.models.StoryModel;
+
+import java.util.ArrayList;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        return view;
+    }
+}
