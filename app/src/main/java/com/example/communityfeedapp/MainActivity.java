03c007bfde2c9650a4762cd1ec2d25eee578d96a
NAJEEB <najeebkhannk123@yahoo.com>
2022-02-04 19:06:26 +0500

    Successfully completed profile screen

--- app/src/main/java/com/example/communityfeedapp/MainActivity.java
+++ app/src/main/java/com/example/communityfeedapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.communityfeedapp;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,10 +28,13 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        setSupportActionBar(binding.toolbar);
+        MainActivity.this.setTitle("My Profile");
         // When we open the application first
         // time the fragment should be shown to the user
         // in this case it is home fragment
         FragmentTransaction defaultFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        binding.toolbar.setVisibility(View.GONE);
         defaultFragmentTransaction.replace(R.id.container_framelayout, new HomeFragment());
         defaultFragmentTransaction.commit();
 
@@ -40,23 +45,34 @@ public class MainActivity extends AppCompatActivity {
 
             switch (itemId) {
                 case R.id.item_home:
+                    binding.toolbar.setVisibility(View.GONE);
                     fragmentTransaction.replace(R.id.container_framelayout, new HomeFragment());
                     break;
                 case R.id.item_notification:
+                    binding.toolbar.setVisibility(View.GONE);
                     fragmentTransaction.replace(R.id.container_framelayout, new NotificationFragment());
                     break;
                 case R.id.item_add_post:
+                    binding.toolbar.setVisibility(View.GONE);
                     fragmentTransaction.replace(R.id.container_framelayout, new AddPostFragment());
                     break;
                 case R.id.item_search:
+                    binding.toolbar.setVisibility(View.GONE);
                     fragmentTransaction.replace(R.id.container_framelayout, new SearchFragment());
                     break;
                 case R.id.item_userprofile:
+                    binding.toolbar.setVisibility(View.VISIBLE);
                     fragmentTransaction.replace(R.id.container_framelayout, new ProfileFragment());
                     break;
             }
             fragmentTransaction.commit();
             return true;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items,menu);
+        return true;
     }
 }
