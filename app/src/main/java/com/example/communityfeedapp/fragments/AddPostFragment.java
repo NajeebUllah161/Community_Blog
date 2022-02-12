2559f86f269e94eca95fffd74de29ea048966d90
NAJEEB <najeebkhannk123@yahoo.com>
2022-02-12 20:03:38 +0500

    completed create post UI

--- app/src/main/java/com/example/communityfeedapp/fragments/AddPostFragment.java
+++ app/src/main/java/com/example/communityfeedapp/fragments/AddPostFragment.java
@@ -1,19 +1,26 @@
 package com.example.communityfeedapp.fragments;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
-
 import com.example.communityfeedapp.R;
+import com.example.communityfeedapp.databinding.FragmentAddPostBinding;
 
 public class AddPostFragment extends Fragment {
+
+    FragmentAddPostBinding binding;
 
     public AddPostFragment() {
         // Required empty public constructor
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,35 @@ public class AddPostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        binding = FragmentAddPostBinding.inflate(inflater,container,false);
+
+        binding.postDescription.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+        return binding.getRoot();
     }
 }
